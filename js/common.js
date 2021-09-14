8303c47eaa26094e713a008213cb9eb48fde095e
donkatz <82435693+donkatz@users.noreply.github.com>
2021-09-14 17:54:56 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,9 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".tabBtn-box li").on(".click", function () {
         var data = $(this).attr("data-rel");
         var $content = $("." + data);
         $(this).addClass("on").siblings().removeClass("on");
         $contnent.show().siblings().hide();
     });
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        slidesPerGroup: 4,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 
 });
