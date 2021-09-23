4ecd8875e873290f0ae44ee649deb2482e67394f
donkatz <82435693+donkatz@users.noreply.github.com>
2021-09-23 18:43:57 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -22,6 +22,72 @@ $(document).ready(function () {
         },
       });
 
+
+
+      // 익준
+      $(".layerPopup01").click(function(){
+        $(".layer-bg").stop().show();
+        $(".layer01").stop().show();
+      });
+      $(".layer01 .close-btn").click(function(){
+        $(".layer-bg").stop().hide();
+        $(".layer01").stop().hide();
+      });
+
+      // 정원
+      $(".layerPopup02").click(function(){
+        $(".layer-bg").stop().show();
+        $(".layer02").stop().show();
+      });
+      $(".layer02 .close-btn").click(function(){
+        $(".layer-bg").stop().hide();
+        $(".layer02").stop().hide();
+      });
+
+      // 준완
+      $(".layerPopup03").click(function(){
+        $(".layer-bg").stop().show();
+        $(".layer03").stop().show();
+      });
+      $(".layer03 .close-btn").click(function(){
+        $(".layer-bg").stop().hide();
+        $(".layer03").stop().hide();
+      });
+
+      // 석형
+      $(".layerPopup04").click(function(){
+        $(".layer-bg").stop().show();
+        $(".layer04").stop().show();
+      });
+      $(".layer04 .close-btn").click(function(){
+        $(".layer-bg").stop().hide();
+        $(".layer04").stop().hide();
+      });
+
+      // 송화
+      $(".layerPopup05").click(function(){
+        $(".layer-bg").stop().show();
+        $(".layer05").stop().show();
+      });
+      $(".layer05 .close-btn").click(function(){
+        $(".layer-bg").stop().hide();
+        $(".layer05").stop().hide();
+      });
+
+      // 동영상 재생
+
+      $("#pause-btn").click(function(){
+        $("#theVideo").get(0).pause();
+        $("#play-btn").addClass("on");
+        $("#pause-btn").addClass("on");
+      });
+      
+      $("#play-btn").click(function(){
+        $("#theVideo").get(0).play();
+        $("#play-btn").removeClass("on");
+        $("#pause-btn").removeClass("on");
+      });
+
       
 
 });
